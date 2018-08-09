c8fd38e6172ac8e44e780c4bab5c900553dfd838
laetitia69 <titi-fernandes@hotmail.fr>
2018-08-09 22:17:14 +0200

    version test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,17 +1,22 @@
 class MoviesController < ApplicationController
   def search
-     # @movie = Movie.new
-  	#  @search = Tmdb::Search.new
+     @title = params[:movie]
+    #  # @movie = Movie.new
+  	 # @search = Tmdb::Search.new
   	# #@search.resource('person') # determines type of resource
   	# @search.query(@movie.title) # the query to search against
   	# @search.fetch # makes request
-    #movie = TmdbMovie.find(:title => params[:search])
+    @movie = Tmdb::Movie.find(@title)
  
   # @movie.title = movie.title
   # @movie.description - movie.description
   # @movie.save
+#   @search = Tmdb::Search.new
+# @search.resource('original_title') # determines type of resource
+# @search.query('params[:movie]') # the query to search against
+# @search.fetch # makes request
 
-  @search = params[:Search]
+  
   #@movies = Movie.find_by(name: @search)
   end
 
